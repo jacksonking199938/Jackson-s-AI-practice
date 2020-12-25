8ff79f33bab062088f15e1077cd7bcdddd0ef25b
nixonjin <2465004358@qq.com>
2020-12-25 18:58:15 +0800

    2020/12/25

--- deep learning/nlp_pretraining/word2vec.py
+++ deep learning/nlp_pretraining/word2vec.py
@@ -107,3 +107,75 @@ def get_negatives(all_contexts, corpus, K):
     return all_negatives
 
 all_negatives = get_negatives(all_contexts, corpus,5)
+#%%
+def batchify(data):
+    max_len = max(len(c) + len(n) for _, c, n in data)
+    centers, contexts_negatives, masks, labels = [],[],[],[]
+    for center, context, negative in data:
+        cur_len = len(context) + len(negative)
+        centers += [center]
+        contexts_negatives += [context + negative + [0] * (max_len - cur_len)]
+        masks += [[1] * cur_len + [0] * (max_len - cur_len)]
+        labels += [[1] * len(context) + [0] * (max_len - len(context))]
+    return (d2l.reshape(torch.tensor(centers),(-1,1)), torch.tensor(contexts_negatives),\
+                torch.tensor(masks), torch.tensor(labels))
+
+
+def load_data_ptb(batch_size, max_window_size, num_noise_words):
+    num_worders = d2l.get_dataloader_workers()
+    sentences = read_ptb()
+    vocab = d2l.Vocab(sentences,min_freq=10)
+    subsampled = subsampling(sentences, vocab)
+    corpus = [ vocab[line] for line in subsampled]
+    all_centers, all_contexts = get_centers_and_contexts(corpus, max_window_size)
+    all_negatives = get_negatives(all_contexts, corpus, num_noise_words)
+
+    class PTBDataset(torch.utils.data.Dataset):
+        def __init__(self, centers, contexts, negatives):
+            assert len(centers) == len(contexts) == len(negatives)
+            self.centers = centers
+            self.contexts = contexts
+            self.negatives = negatives
+        
+        def __getitem__(self, index):
+            return (self.centers[index], self.contexts[index], self.negatives[index])
+        
+        def __len__(self):
+            return len(self.centers)
+
+    dataset = PTBDataset(all_centers, all_contexts, all_negatives)
+    data_iter = torch.utils.data.DataLoader(dataset, batch_size,shuffle=True,
+                                            collate_fn=batchify,
+                                            num_worders=num_worders)
+    return data_iter, vocab
+
+#%%
+data_iter,vocab = load_data_ptb(512,5,5)
+for batch in data_iter:
+    for name,data in zip(names, batch):
+        print(name, 'shape:', data.shape)
+    break
+# %% pretraining word2vec
+
+from d2l import torch as d2l
+import torch
+from torch import nn
+
+batch_size, max_window_size, num_noise_words = 512, 5, 5
+data_iter, vocab = d2l.load_data_ptb(batch_size, max_window_size,
+                                    num_noise_words)
+
+embed = nn.Embedding(num_embeddings=20, embedding_dim=4)
+print(f'Parameter embedding_weight ({embed.weight.shape}, '
+      'dtype={embed.weight.dtype})')
+
+x = torch.tensor([[1,2,3],[4,5,6]])
+embed(x)
+
+#%%
+class SigmoidBCELoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, inputs, target, mask=None):
+        out = nn.functional.binary_cross_entropy_with_logits()
