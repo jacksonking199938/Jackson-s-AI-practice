149d007fb24ddc38df1bdb8fe395473d44b66f94
nixonjin <2465004358@qq.com>
2020-12-31 18:52:38 +0800

    update 2020/12/31

---
+++ deep learning/nlp_pretraining/bert.py
@@ -0,0 +1,24 @@
+#%%
+from d2l import torch as d2l
+from torch import nn 
+#%%
+def get_tokens_and_segments(tokens_a, tokens_b=None):
+    tokens = ['<cls>'] + tokens_a + ['<sep>']
+    # 0 and 1 are marking segment A and B, respectively
+    segments = [0] * (len(tokens_a)+2)
+    if tokens_b is not None:
+        tokens += tokens_b + ['<sep>']
+        segments += [1] * (len(tokens_b) + 1)
+    return tokens, segments
+
+class BERTEncoder(nn.Module):
+    def __init__(self, vocab_size, num_hiddens, ffn_num_hiddens, num_heads,
+                num_layers, dropout, max_len=1000, **kwargs):
+        super(BERTEncoder, self).__init__(**kwargs)
+        self.token_embedding = nn.Embedding(vocab_size, num_hiddens)
+        self.segment_embedding = nn.Embedding(2, num_hiddens)
+        self.blks = nn.Sequential()
+        for _ in range(num_layers):
+            self.blks.add_module(d2l.EncoderBlock(
+                num_hiddens,num_hiddens,num_hiddens, ffn_num_hiddens, num_heads, dropout, True
+            ))
